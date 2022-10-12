13b1e15ed07b189eae960639fc58a3e78cd91993
Riyaz J <jriyaz@zeomega.com>
2022-10-12 22:50:03 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const {
+  shareAll,
+  withModuleFederationPlugin,
+} = require("@angular-architects/module-federation/webpack");
+
+module.exports = withModuleFederationPlugin({
+  name: "user",
+
+  exposes: {
+    "./profile": "./projects/user/src/app/profile/profile.module.ts",
+  },
+
+  shared: {
+    ...shareAll({
+      singleton: true,
+      strictVersion: true,
+      requiredVersion: "auto",
+    }),
+  },
+});
